5fc5d4eb03cb439ec95fe3f6bbdc8c20757510b6
mf3660147-source <mf3660147@gmail.com>
2026-08-27 19:46:28 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,7 +22,8 @@ client.on('guildMemberAdd', async (member) => {
     }
 });
 
-client.login('MTU0MjQzMzk1NTM1NjA4MjIxOA.G5o6Jg.bM_hS0dmi7DAwXueqL-OicMHAk6uVetFrer-2A');
+client.login(process.env.DISCORD_TOKEN);
+;
 ;
 
 
