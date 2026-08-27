11dfa21abf5884f272161d5ca627ff5532afb81f
mf3660147-source <mf3660147@gmail.com>
2026-08-27 18:46:01 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,26 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const express = require('express');
+
+const app = express();
+app.get('/', (req, res) => res.send('Bot is Live!'));
+app.listen(3000, () => console.log('Server Ready'));
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers
+    ]
+});
+
+client.on('guildMemberAdd', async (member) => {
+    try {
+        const roleId = '1542503336484413460'; 
+        const role = member.guild.roles.cache.get(roleId);
+        if (role) await member.roles.add(role);
+    } catch (err) {
+        console.error(err);
+    }
+});
+
+client.login('MTU0MjQ0NzM5MDMwNzM5MzU0Ng.GVxlBE.KTgddw4ul_Is7cACbfA2ZvgKgYDOKwHosERaS8');
 
