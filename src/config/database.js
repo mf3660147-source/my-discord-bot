3fdd41de28e686193af0add7c9234283a459c22a
mf3660147-source <mf3660147@gmail.com>
2026-09-05 21:42:17 +0530

    Add src/config/database.js

---
+++ src/config/database.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql2/promise');
+
+// ---------------- MySQL DATABASE CONNECTION POOL ----------------
+const db = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: parseInt(process.env.DB_PORT) || 3306,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+    enableKeepAlive: true,
+    keepAliveInitialDelay: 10000
+});
+
+module.exports = db;
